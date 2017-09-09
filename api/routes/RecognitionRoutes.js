17ff950c9b08a5696783c3373337b04fb81c7c65
Aditya Paruchuri <ap376@duke.edu>
2017-09-09 01:19:30 -0400

    added new route

--- api/routes/RecognitionRoutes.js
+++ api/routes/RecognitionRoutes.js
@@ -1,9 +1,10 @@
 'use strict';
 module.exports = function(app) {
   var getData = require('../controllers/RecognitionController');
-
-  // todoList Routes
   app.route('/register')
     .get(getData.returnNewUserOrNot);
     .post(getData.createNewUser);
+  
+  app.route('/recognize')
+    .get(getData.returnIdentity);
 };
