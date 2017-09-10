4b4b10b218c70ea1e54190dbfc80a7cc3702f94c
Mustafa Abdelhamid <abdelha5@uwm.edu>
2017-09-09 23:29:46 -0400

    async problems

--- server.js
+++ server.js
@@ -145,17 +145,15 @@ app.get("/reset", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      console.log(data);
+      rekognition.createCollection({ "CollectionId": "TestCollection" }, function (err, data) {
+        if (err) {
+          console.log(err, err.stack); // an error occurred
+        } else {
+          console.log(data);           // successful response
+        }
+      });
     }
   })
-
-  rekognition.createCollection( { "CollectionId": "TestCollection"}, function(err, data) {
-    if (err) {
-    console.log(err, err.stack); // an error occurred
-    } else {
-    console.log(data);           // successful response
-    }
-  });
 });
 
 app.put("/editPatient/:id", function(req, res) {
