3b7ad6a73e3a723142436875dc1014faecbbae8b
Mustafa Abdelhamid <abdelha5@uwm.edu>
2017-09-09 23:32:51 -0400

    s

--- server.js
+++ server.js
@@ -145,15 +145,19 @@ app.get("/reset", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      rekognition.createCollection({ "CollectionId": "TestCollection" }, function (err, data) {
-        if (err) {
-          console.log(err, err.stack); // an error occurred
-        } else {
-          console.log(data);           // successful response
-        }
-      });
+      console.log(data);
     }
   })
+});
+
+app.get("/createCollection", function(req, res) {
+  rekognition.deleteCollection({"CollectionId" : config.collectionName}, function(err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	});
 });
 
 app.put("/editPatient/:id", function(req, res) {
